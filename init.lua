c6cee7a0470b75dce283121e73ec34a798ea586b
Artur B <artur.p.bebenek@gmail.com>
2025-08-12 12:47:05 +0100

    Continue to config LSP

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 vim.o.number = true
+vim.o.relativenumber = true
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
@@ -66,12 +67,50 @@ require("lazy").setup({
       build = ":TSUpdate",
     },
     {
-      "mason-org/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
       opts = {},
       dependencies = {
         { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-      },
+        "mason-org/mason-lspconfig.nvim",
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        },
+       config = function()
+         vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+          callback = function(event)
+            local map = function(keys, func, desc, mode)
+              mode = mode or 'n'
+              vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+            end
+            map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+            map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+            map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+            
+          end
+         })
+
+         local servers = {
+           lua_ls = {}
+         }
+
+         local ensure_installed = vim.tbl_keys(servers or {})
+         vim.list_extend(ensure_installed, {})
+         require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+         require('mason-lspconfig').setup {
+           ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+           automatic_installation = false,
+           handlers = {
+             function(server_name)
+               local server = servers[server_name] or {}
+               server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+               require('lspconfig')[server_name].setup(server)
+             end,
+           },
+         }
+       end
+
     },
   },
 })
